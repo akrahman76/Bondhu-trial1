660f3ae9ba7ccd698cf6e67977ce702934627b7f
akrahman76 <42632725+akrahman76@users.noreply.github.com>
2021-11-15 16:42:01 +0600

    templates

--- bondhu/main/views.py
+++ bondhu/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(response):
+    return render(response, "main/home.html",{})
